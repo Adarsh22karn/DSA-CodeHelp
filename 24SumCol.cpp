463ad88f98b1b771ea1d066dc31d9d172f91cbff
Adarsh22karn <113933982+Adarsh22karn@users.noreply.github.com>
2026-01-29 23:11:33 +0530

    Add files via upload

---
+++ 24SumCol.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<limits>
+using namespace std;
+
+
+
+
+void printA(int arr[][3],int row,int col)
+{
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+			cout<<arr[i][j]<<" ";	
+		}
+		cout<<endl;
+	}
+}
+
+
+
+
+
+
+void sum(int arr[][3], int row, int col)
+{
+	
+	for(int i=0;i<col;i++)
+	{
+		int sum=0;
+		for(int j=0;j<row;j++)
+		{
+			sum=sum+arr[j][i];
+		}
+		cout<<"Sum Of each column: "<<sum<<endl;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+	int arr[3][3];
+	int row=3,col=3;
+
+	
+	for (int i=0;i<row;i++)
+	{
+		cout<<"Enter the valve at"<<i<<" row "<<endl;
+		for (int j=0;j<col;j++)
+		{
+			cout<<"Enter the valve at "<<j<<" column "<<" :";
+			cin>>arr[i][j];	
+		}
+		cout<<endl;
+	}
+	
+	printA(arr,row,col);
+	sum(arr,row,col);
+	
+}
