2450e28d1e97ae47edd123e0db00de1d9e5d963f
Adarsh22karn <113933982+Adarsh22karn@users.noreply.github.com>
2026-02-01 22:14:56 +0530

    Add files via upload

---
+++ 30InputVector.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+void print(vector<int> arr)
+{
+	cout<<"Print Array: ";
+	int size= arr.size();
+	for(int i=0;i<size;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
+
+
+
+
+
+
+void print2(vector<int> arr)
+{
+	cout<<"Print Array in vector native method : "<<endl;
+	for(auto it: arr)
+	{
+		cout<<it<<" ";
+	}
+	cout<<endl;
+}
+
+
+
+int main()
+{
+	vector<int> arr;
+	int n;
+	cout<<"Enter the size of Array: ";
+	cin>>n;	
+	
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Add data to "<<i<<" Index: ";
+		int d;
+		cin>>d;
+		arr.push_back(d);
+	}
+	
+	
+	print(arr);
+	print2(arr);
+}
