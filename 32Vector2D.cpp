ae221e5cfe6328d53ff7024e9e2a9d63820ee528
Adarsh22karn <113933982+Adarsh22karn@users.noreply.github.com>
2026-02-02 23:18:34 +0530

    Add files via upload

---
+++ 32Vector2D.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	//2D array
+	vector< vector<int> >arr(5, vector<int>(10,0));
+	//row size-> arr.size();
+	for(int i=0;i<arr.size();i++)
+	{
+		for(int j=0;j<arr[i].size();j++)
+		{
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
