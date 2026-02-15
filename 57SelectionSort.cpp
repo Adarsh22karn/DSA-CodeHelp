7c8a4ce03f97f0f66ab8a967f7d6e02f59e363dd
Adarsh22karn <113933982+Adarsh22karn@users.noreply.github.com>
2026-02-15 23:43:06 +0530

    Add files via upload

---
+++ 57SelectionSort.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+
+void SelectionSort(vector<int> &arr ,int n)
+{
+	for(int i=0;i<n-1;i++)
+	{
+		int minIndex=i;//i th element hi smallest hai
+		for(int j=i+1;j<n;j++)
+		{
+			if(arr[j]< arr[minIndex])
+			{
+				minIndex=j;//minIndex ka value update karna hai
+			}
+		}
+		
+		
+		//swap ith and min Index elements
+		swap(arr[i],arr[minIndex]);
+	}
+}
+
+ 
+void print(vector<int> &arr ,int n)
+{
+	cout<<"Array after Sorting: ";
+	for (int i=0;i<n; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
+
+
+
+
+int main()
+{
+	vector<int> arr;
+	int n;
+	cout<<"Enter the size of Array: ";
+	cin>>n;	
+	
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Add data to "<<i<<" Index: ";
+		int d;
+		cin>>d;
+		arr.push_back(d); 
+	}
+	
+	SelectionSort(arr, n);
+	print(arr, n);
+	
+}
