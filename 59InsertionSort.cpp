ea0a6e8dae564cc0e3122de0e092da718f6067b2
Adarsh22karn <113933982+Adarsh22karn@users.noreply.github.com>
2026-02-16 23:35:52 +0530

    Add files via upload

---
+++ 59InsertionSort.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+
+void InsertionSort(vector<int> &arr ,int n)
+{
+	for(int i=0;i<n-1;i++)
+	{
+		int key=arr[i];
+		int j=i-1;
+		//move element of arr[0....i-1]
+		//that are greater than key to one position 
+		//ahead of theier current position
+		while(j>=0 && arr[j]>key)
+		{
+			arr[j+1]= arr[j];
+			j--;
+		}
+		arr[j+1]=key;
+		
+	}
+}
+		
+
+ 
+void print(vector<int> &arr ,int n)
+{
+	cout<<"Array after Sorting: ";
+	for (int i=0;i<n; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
+
+
+
+
+int main()
+{
+	vector<int> arr;
+	int n;
+	cout<<"Enter the size of Array: ";
+	cin>>n;	
+	
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Add data to "<<i<<" Index: ";
+		int d;
+		cin>>d;
+		arr.push_back(d); 
+	}
+	
+	InsertionSort(arr, n);
+	print(arr, n);
+	
+}
