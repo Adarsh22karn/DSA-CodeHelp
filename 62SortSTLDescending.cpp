dac110723fc53fdbded91c867eeba9e8b0ca3ee6
Adarsh22karn <113933982+Adarsh22karn@users.noreply.github.com>
2026-02-17 22:56:07 +0530

    Add files via upload

---
+++ 62SortSTLDescending.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+
+
+//function to print array
+void print(vector<int> &arr ,int n)
+{
+	cout<<"Array after Sorting: ";
+	for (int i=0;i<n; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
+
+
+
+
+bool mycomp(int &a, int&b)
+{
+	return a>b;
+}
+
+int main()
+{
+	vector<int> arr;
+	int n;
+	cout<<"Enter the size of Array: ";
+	cin>>n;	
+	
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Add data to "<<i<<" Index: ";
+		int d;
+		cin>>d;
+		arr.push_back(d); 
+	}
+	
+	// third comperatue is for ki user bataya kaisa sort karn hai 
+	// by defauly wo phala se ascending hota hai
+	
+	sort(arr.begin() , arr.end(), mycomp);
+	
+	print(arr, n);
+	
+}
+	
+	
